4bd236e762fed2cd61aff7cd33a5456c31209e0c
prayugo-setioko <prayugo.setioko@allianz.co.id>
2025-03-07 20:36:07 +0700

    [Feat] update penanda project

--- src/components/card/cardComponent.tsx
+++ src/components/card/cardComponent.tsx
@@ -1,18 +1,57 @@
 import Image from "next/image"
+import { Crown, LayoutTemplate, MonitorSmartphone } from "lucide-react"; // Import ikon
 
 interface ICardComponent {
     contentProject: { id: number; tag: string[] | string ; src: string | any, url:string }[];
 }
 
-export const CardComponent= ({contentProject}:ICardComponent) => {
+export const CardComponent = ({ contentProject }: ICardComponent) => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10 lg:px-[4%]">
-            {contentProject.map((value) =>(
-                <a key={value.id} href={value.url} className="m-auto" target="_blank">
-                <Image alt={value.tag[0]} loading="lazy" width="1280" height="720" decoding="async" data-nimg="1" 
-                    className="rounded-xl" src={value.src} />            
-                </a>
-            ))}
+            {contentProject.map((value) => {
+                const tags = Array.isArray(value.tag) ? value.tag : [value.tag];
+
+                const isPremium = tags.includes("Premium");
+                const isTemplate = tags.includes("Template");
+                const isApplication = tags.includes("Application");
+                return (
+                    <a key={value.id} href={value.url} className="relative m-auto" target="_blank">
+                         <div className="absolute top-2 right-2 flex flex-col gap-1">
+                            {isPremium && (
+                                <div className="bg-red-700 text-white text-xs font-bold px-3 py-1 flex items-center gap-1 rounded-md shadow-md">
+                                    <Crown size={14} className="text-white" />
+                                    PREMIUM
+                                </div>    
+                            )}
+                            
+                            {isTemplate && (
+                                <div className="bg-blue-500 text-white text-xs font-bold px-3 py-1 flex items-center gap-1 rounded-md shadow-md">
+                                    <LayoutTemplate size={14} className="text-white" />
+                                    TEMPLATE
+                                </div>
+                            )}
+                                
+                            {isApplication && (
+                                <div className="bg-green-500 text-white text-xs font-bold px-3 py-1 flex items-center gap-1 rounded-md shadow-md">
+                                    <MonitorSmartphone size={14} className="text-white" />
+                                    APPLICATION
+                                </div>    
+                            )}
+                            
+                        </div>
+                        <Image
+                            alt={Array.isArray(value.tag) ? value.tag[0] : value.tag}
+                            loading="lazy"
+                            width="1280"
+                            height="720"
+                            decoding="async"
+                            data-nimg="1"
+                            className="rounded-xl"
+                            src={value.src}
+                        />
+                    </a>
+                );
+            })}
         </div>
-    )
-}
+    );
+};
--- src/mocks/projectsContent.ts
+++ src/mocks/projectsContent.ts
@@ -5,26 +5,26 @@ import ylcHoroscop from "../../src/assets/projects/Ylchoroscop.png"
 export const projectsSample = [
     {
         id: 1,
-        tag: ["HTML","CSS"],
+        tag: ["HTML","CSS","Template"],
         src: spaceTourism,
         url:"https://www.frontendmentor.io/solutions/responsive-space-tourism-multipage-apps-using-html-css-and-javasc-I5wQpGawLX"
     },
     {
         id: 2,
-        tag: ["HTML","CSS"],
+        tag: ["HTML","CSS","Template"],
         src: newsHomePage,
         url:"https://www.frontendmentor.io/solutions/responsive-news-homepage-using-html-css-and-javascript-Gn6Kc6DkdT"
     },
     {
         id:3,
-        tag:["NEXT.JS","React.js","Tailwind"],
+        tag:["NEXT.JS","React.js","Tailwind","Application"],
         src:chatAi,
         url:"https://chat-ai-nextjs-9icqyqa82-prays-projects-e48c2c66.vercel.app/"
 
     },
     {
         id:4,
-        tag:["HTML","CSS","Bootstrap"],
+        tag:["HTML","CSS","Bootstrap","Template"],
         src:ylcHoroscop,
         url:"https://your-life-choice.netlify.app/"
 
@@ -60,5 +60,17 @@ export const tagProjectsSample = [
     {
         id:7,
         tag:"Bootstrap"
-    }
+    },
+    {
+        id:8,
+        tag:"Template"
+    },
+    {
+        id:9,
+        tag:"Application"
+    },
+    {
+        id:10,
+        tag:"Premium"
+    },
 ]
