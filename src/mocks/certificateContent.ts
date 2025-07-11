abc352ca639aaa742d6f08066d5bf153f9004790
Prayugo <dwi.deni24@gmail.com>
2025-07-12 01:05:02 +0700

    [Feat] add new certificate

--- src/mocks/certificateContent.ts
+++ src/mocks/certificateContent.ts
@@ -6,59 +6,68 @@ import ReactDesign from "../../src/assets/certificate/react_design.jpeg"
 import ReactTesting from "../../src/assets/certificate/react_testing_and_debuging.jpeg"
 import TailwindCss from "../../src/assets/certificate/Tailwind_CSS.jpeg"
 import TypescripEsential from "../../src/assets/certificate/Typescript_esential.jpeg"
+import awsStorage from "../../src/assets/certificate/aws-storage.png"
+import awsCloud101 from "../../src/assets/certificate/aws-introduce.png"
 
 export const certificateSample = [
     {
-        id: 1,
-        tag: "AI",
-        title:"AI Chat Prompts for Business Analysis",
-        src: AiBusiness,
-        url:"https://www.linkedin.com/learning/certificates/4cc6914f3c2e96375121a082d6236f2d1c2885917ab2f213ecb3d4def523e524?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BJjUhuG%2BQSeSRtqM2CyM7zg%3D%3D"
+        id:1,
+        tag: "AWS",
+        title:"AWS Educate Getting Started with Storage",
+        src: awsStorage,
+        url:"https://www.credly.com/badges/dd37c8dd-d9cb-404a-b62d-8b18db196cb7/public_url"
     },
     {
         id: 2,
+        tag: "AWS",
+        title:"AWS Educate Introduction to Cloud 101",
+        src: awsCloud101,
+        url:"https://www.credly.com/badges/880c0ae7-4a76-4eb3-b447-335ab5d010bd/public_url"
+    },
+    {
+        id: 3,
         tag: "Digital",
         title:"Digital Skills self-assasment test result",
         src: DigitalSkils,
         url:"https://www.linkedin.com/in/prayugo-dwi-setiyoko/details/certifications/1728387560275/single-media-viewer/?profileId=ACoAACaF4AkBEEO1YU23dtTOPrnFwnkztpW3WHg"
     },
     {
-        id: 3,
+        id: 4,
         tag: "Docker",
         title:"Docker for Developers",
         src: Docker,
         url:"https://www.linkedin.com/learning/certificates/d94a5a697d1cb9b9fbd21eabc4dc914feede67268787cc2760ad6cf73b442c2d?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BJjUhuG%2BQSeSRtqM2CyM7zg%3D%3D"
     },
     {
-        id: 4,
+        id: 5,
         tag: "java",
         title:"Java Basic",
         src: java,
         url:"https://www.hackerrank.com/certificates/iframe/a9f381d2e892"
     },
     {
-        id: 5,
+        id: 6,
         tag: "React",
         title:"React: Design Patterns",
         src: ReactDesign,
         url:"https://www.linkedin.com/learning/certificates/0294bb6a5ed1b5bd1106f70b477a7844cbfd1b3ec51a75e1405a4e6f9fb5c7e8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BJjUhuG%2BQSeSRtqM2CyM7zg%3D%3D"
     },
     {
-        id: 6,
+        id: 7,
         tag: "React",
         title:"React: Testing and Debugging",
         src: ReactTesting,
         url:"https://www.linkedin.com/learning/certificates/f1bd82375a7ef24765344f3cf982cefaee863f121e1225493c5496aef2991f3f?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BJjUhuG%2BQSeSRtqM2CyM7zg%3D%3D"
     },
     {
-        id: 7,
+        id: 8,
         tag: "CSS",
         title:"Tailwind CSS Essential Training",
         src: TailwindCss,
         url:"https://www.linkedin.com/learning/certificates/408d17b6d15babaa3667e0c4536eb7fdc0ea627de56a17eccc81a8378a060619?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BJjUhuG%2BQSeSRtqM2CyM7zg%3D%3D"
     },
     {
-        id: 8,
+        id: 9,
         tag: "Typescript",
         title:"TypeScript Essential Training",
         src: TypescripEsential,
